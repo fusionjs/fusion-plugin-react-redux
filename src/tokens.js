b876931b719fd26853fc10ec1c4880e03fa2119a
Leo Horie <lhorie@uber.com>
2018-01-19 11:13:21 -0800

    expose tokens and update docs

--- src/tokens.js
+++ src/tokens.js
@@ -1,5 +1,6 @@
 import {createToken} from 'fusion-tokens';
 
+export const ReduxToken = createToken('ReduxToken');
 export const ReducerToken = createToken('ReducerToken');
 export const PreloadedStateToken = createToken('PreloadedStateToken');
 export const EnhancerToken = createToken('EnhancerToken');
